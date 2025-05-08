a57a5b0ec0d784ef946556a6ae5d5e50fbd39ccd
No Name <93629090+fnhg1ab0@users.noreply.github.com>
2025-05-09 06:28:46 +0700

    Delete src/server directory

--- src/server/src/routes/recordings.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const recordingController = require('../controllers/recordingController');
-
-router.get('/', recordingController.getRecordings);
-router.post('/webhook', recordingController.handleWebhook);
-
-module.exports = router;
